2451eab1b8464d07f6f4f5fc7b9a254824006290
Ryan Anderson <ryanaanderson0@gmail.com>
2022-05-27 07:49:22 -0700

    added material ui library & navbar component/styles

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -6,7 +6,27 @@ import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import { createTheme } from '@mui/material/styles';
+import Raleway from '../../assets/fonts/Raleway-VariableFont_wght.ttf';
 
+const theme = createTheme({
+  typography: {
+    fontFamily: {
+      fontFamily: 'Raleway',
+    },
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: `
+          @font-face {
+            font-family: 'Raleway';
+            font-style: normal;
+            font-weight: 500;
+            src: local('Raleway'), local('Raleway-Regular'), url(${Raleway}) format('ttf');
+          }
+        `,
+      },
+    },
+  },
+});
 
 const Navbar = () => {
   return (
@@ -21,7 +41,13 @@ const Navbar = () => {
           >
         
         </IconButton>
-          <Typography>Hello</Typography>
+          <Typography
+            sx={{
+              fontFamily: 'Raleway',
+              fontSize: '2rem'
+            }}
+            variant="h3"
+            my={1}>Heckle Vision</Typography>
         </Toolbar>  
     </AppBar>
 
